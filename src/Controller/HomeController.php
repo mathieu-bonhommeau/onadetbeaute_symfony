08edb15d5f0abd6863fede8339eb2800e7b76807
matanna <mat.bonhommeau85@gmail.com>
2022-01-17 23:53:14 +0100

    integrate presentation slider with animations

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\PhotoRepository;
 use App\Repository\PrestationTypeRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(PrestationTypeRepository $prestationTypeRepository): Response
+    public function home(PrestationTypeRepository $prestationTypeRepository, PhotoRepository $photoRepository): Response
     {
         $prestationTypes = $prestationTypeRepository->findAll();
-
+        $frontPhoto = $photoRepository->findOneBy(['frontPhoto' => true]);
+        
         return $this->render('home/home.html.twig', [
             'prestationTypes' => $prestationTypes,
+            'frontPhoto' => $frontPhoto
         ]);
     }
 }
