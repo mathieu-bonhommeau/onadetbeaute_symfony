f91fd202bdf03a116ca40d5fbc7c0d2e43bcf18e
matanna <mat.bonhommeau85@gmail.com>
2022-01-19 21:15:13 +0100

    integrate presentation section with slider

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,23 +2,43 @@
 
 namespace App\Controller;
 
-use App\Repository\PhotoRepository;
-use App\Repository\PrestationTypeRepository;
+use App\Entity\Photo;
+use App\Entity\OnadEtBeaute;
+use App\Entity\PrestationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    private $manager;
+
+    public function __construct(EntityManagerInterface $manager) {
+        $this->manager = $manager;
+    }
+
     #[Route('/', name: 'home')]
-    public function home(PrestationTypeRepository $prestationTypeRepository, PhotoRepository $photoRepository): Response
+    public function home(): Response
     {
-        $prestationTypes = $prestationTypeRepository->findAll();
-        $frontPhoto = $photoRepository->findOneBy(['frontPhoto' => true]);
+
+        $prestationTypes = $this->manager->getRepository(PrestationType::class)
+            ->findAll();
+
+        $principalPhoto = $this->manager->getRepository(Photo::class)
+            ->findOneBy(['principalPhoto' => true]);
+        
+        $frontSliderPhotos = $this->manager->getRepository(Photo::class)
+            ->findBy(['frontPhoto' => true]);
+
+        $onadetbeaute = $this->manager->getRepository(OnadEtBeaute::class)
+            ->findAll()[0];
         
         return $this->render('home/home.html.twig', [
             'prestationTypes' => $prestationTypes,
-            'frontPhoto' => $frontPhoto
+            'principalPhoto' => $principalPhoto,
+            'onadetbeaute' => $onadetbeaute,
+            'frontSliderPhotos' => $frontSliderPhotos
         ]);
     }
 }
