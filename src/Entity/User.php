575c057c070fe5523d73d64c6b735fd8a889f9c8
Mathieu Bonhommeau <mat.bonhommeau85@gmail.com>
2022-04-28 01:07:45 +0200

    change annotation table in entity user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,7 +9,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="user")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
